a813540a217c78a6b3b4a62e4ce3a66dd7e531ca
alexandru.drop <alexandru.drop@gmail.com>
2024-03-08 18:03:20 +0200

    curs3 commit final

---
+++ src/curs3/DataTypeExamples.java
@@ -0,0 +1,25 @@
+package curs3;
+
+public class DataTypeExamples {
+
+	public static void main(String[] args) {
+		// data type numeric de tip intreg
+		byte byteNumber = 127; // -128--> 127
+		short shortNumber = 32767; //-32768 - 32767
+		int intNumber = 2147483647; //-2147483648 - 2147483647
+		long longNumber = 922334343432242L; 
+		
+		//float
+		double doubleNumber = 3.14159265359; //15 zecimale
+		float floatNumber = 3.141592f; //6-7 zecimale
+		
+		
+		double calcul = 5.6 + 5.8;
+		System.out.println(calcul);
+		
+		char character = '#'; //caracter unicode
+		boolean boolValue = true; // valoare true sau false
+
+	}
+
+}

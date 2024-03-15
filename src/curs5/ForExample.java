5088960b1ef294dfeb731a74d237a352136b9776
alexandru.drop <alexandru.drop@gmail.com>
2024-03-15 16:39:05 +0200

    curs 5 intermediar

--- src/curs5/ForExample.java
+++ src/curs5/ForExample.java
@@ -3,7 +3,19 @@ package curs5;
 public class ForExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		
+		for(int i = 0; i < 10; i++) {
+			
+			if(i==5) {
+				//break;
+				continue;
+			}
+			
+			System.out.println("Valoarea lui i este " + i);
+		}
+		
 
 	}
 

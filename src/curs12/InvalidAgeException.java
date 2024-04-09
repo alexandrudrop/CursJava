09dcae871fb5359e70a33b4efdc0d6c26657e3f0
alexandru.drop <alexandru.drop@gmail.com>
2024-04-09 18:01:40 +0300

    curs12 final

---
+++ src/curs12/InvalidAgeException.java
@@ -0,0 +1,9 @@
+package curs12;
+
+public class InvalidAgeException extends Exception{
+	
+	public InvalidAgeException(String mesaj) {
+		super(mesaj);
+	}
+
+}
---
+++ src/curs12/TestAgeVerifier.java
@@ -0,0 +1,26 @@
+package curs12;
+
+import java.util.Scanner;
+
+public class TestAgeVerifier {
+
+	public static void main(String[] args) {
+		
+		AgeVerifier obj = new AgeVerifier();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("PLease enter your age:");
+		int age = scan.nextInt();
+		
+		try {
+			obj.checkAgeLimit(age);
+		} catch (InvalidAgeException e) {
+			//e.printStackTrace();
+			System.out.println(e.getMessage());
+			System.out.println("Invalid age to vote!");
+		}
+		
+		System.out.println("Code after");
+
+	}
+
+}
